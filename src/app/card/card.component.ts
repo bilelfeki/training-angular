031c86b7349146c0a5e533fc2895c380cf93287f
ng-bilel <bilel.feki.92@gmail.com>
2025-02-08 10:38:38 +0100

    replace the component logic in the service

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TransformerService } from '../transformer.service';
 
 @Component({
   selector: 'app-card',
@@ -8,8 +9,11 @@ import { Component } from '@angular/core';
   styleUrl: './card.component.css'
 })
 export class CardComponent {
+  constructor(private transformer:TransformerService){
+
+  }
 toUppercase() {
-  this.title = this.title.toUpperCase();
+  this.title = this.transformer.transformSimpleStringToUppercase(this.title);
 }
   title="Noteworthy technology acquisitions 2021";
   cardText="Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.";
