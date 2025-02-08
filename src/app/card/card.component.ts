6ffc05f4fd4107cfb1ed781ef725653fa51ac061
ng-bilel <bilel.feki.92@gmail.com>
2025-02-08 10:29:01 +0100

    make component tiltle and text dynamic

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -8,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './card.component.css'
 })
 export class CardComponent {
+  title="Noteworthy technology acquisitions 2021";
+  cardText="Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.";
 
 }
