f34bba088628decb31d21f01409a68926555bf8e
ng-bilel <bilel.feki.92@gmail.com>
2025-02-08 10:33:50 +0100

    add the possibility to uppercase the title

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -8,7 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './card.component.css'
 })
 export class CardComponent {
+toUppercase() {
+  this.title = this.title.toUpperCase();
+}
   title="Noteworthy technology acquisitions 2021";
   cardText="Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.";
-
 }
